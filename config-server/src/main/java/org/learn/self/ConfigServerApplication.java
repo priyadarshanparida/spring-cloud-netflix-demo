801b79b43f2e90c58acf51a393df320f249f8af8
Priyan Parida <pair+ggiesemann+priyanparida@example.com>
2016-02-09 14:59:18 -0500

    Initial Commit: config-server

---
+++ config-server/src/main/java/org/learn/self/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package org.learn.self;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.EnvironmentController;
+import org.springframework.cloud.config.server.config.EnvironmentRepositoryConfiguration;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
