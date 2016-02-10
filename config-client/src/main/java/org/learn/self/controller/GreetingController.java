670a16308a45baa345a1641604c82baf10e73346
Priyan Parida <priyadarshan.parida@citi.com>
2016-02-09 21:51:02 -0500

    removed unused imports

--- config-client/src/main/java/org/learn/self/controller/GreetingController.java
+++ config-client/src/main/java/org/learn/self/controller/GreetingController.java
@@ -2,7 +2,6 @@ package org.learn.self.controller;
 
 import org.learn.self.service.GreetingProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
--- config-server/src/main/java/org/learn/self/ConfigServerApplication.java
+++ config-server/src/main/java/org/learn/self/ConfigServerApplication.java
@@ -3,8 +3,6 @@ package org.learn.self;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.config.server.EnvironmentController;
-import org.springframework.cloud.config.server.config.EnvironmentRepositoryConfiguration;
 
 @SpringBootApplication
 @EnableConfigServer
