e56ab269a7c494ac2c9e50c349841953db53bfd2
Priyan Parida <priyadarshan.parida@citi.com>
2016-02-10 01:31:52 -0500

    added eureka server and registered config-server and config-client as eureka clients

--- config-client/src/main/java/org/learn/self/ConfigClientApplication.java
+++ config-client/src/main/java/org/learn/self/ConfigClientApplication.java
@@ -2,11 +2,11 @@ package org.learn.self;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientApplication {
-
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
